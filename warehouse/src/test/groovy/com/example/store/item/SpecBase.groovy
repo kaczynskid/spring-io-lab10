d4f7e6c0dcfbf9a41ede1b14117bb597e8ecb6fb
Darek Kaczyński <dariusz.kaczynski@7bulls.com>
2017-11-21 09:54:58 +0100

    Added store

--- warehouse/src/test/groovy/com/example/store/item/SpecBase.groovy
+++ warehouse/src/test/groovy/com/example/store/item/SpecBase.groovy
@@ -1,6 +1,6 @@
 package com.example.store.item
 
-import com.example.store.StoreApplication
+import com.example.store.WarehouseApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
@@ -8,7 +8,7 @@ import spock.lang.Specification
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
-@SpringBootTest(classes = [StoreApplication, ItemTestConfig],
+@SpringBootTest(classes = [WarehouseApplication, ItemTestConfig],
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 class SpecBase extends Specification {
