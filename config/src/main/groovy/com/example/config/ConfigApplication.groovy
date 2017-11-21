993b89bb4eed4b21b3b6edf2ee49ea314a1c4b8d
Darek Kaczyński <dariusz.kaczynski@7bulls.com>
2017-11-21 09:14:46 +0100

    Registered config in registry

--- config/src/main/groovy/com/example/config/ConfigApplication.groovy
+++ config/src/main/groovy/com/example/config/ConfigApplication.groovy
@@ -3,10 +3,12 @@ package com.example.config
 import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 @CompileStatic
 class ConfigApplication {
 
